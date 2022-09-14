c54f82f372717782232923dd1cdc75cb58591fb4
Facundo Martin <facundomartin98@gmail.com>
2022-09-14 16:31:48 -0700

    moved NavItem to UI folder

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -3,33 +3,7 @@ import { useRouter } from 'next/router';
 import React from 'react';
 import ThemeChanger from './ThemeChanger';
 import { clsx } from 'clsx';
-
-type NavItemProps = {
-  children?: React.ReactNode;
-  href: string;
-};
-
-function NavItem({ href, children }: NavItemProps) {
-  let isActive = useRouter().pathname === href;
-
-  return (
-    <li>
-      <Link href={href}>
-        <a
-          className={clsx(
-            'relative block px-3 py-2 transition',
-            isActive ? 'text-teal-500 dark:text-teal-400' : 'hover:text-teal-500 dark:hover:text-teal-400'
-          )}
-        >
-          {children}
-          {isActive && (
-            <span className="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 dark:from-teal-400/0 dark:via-teal-400/40 dark:to-teal-400/0" />
-          )}
-        </a>
-      </Link>
-    </li>
-  );
-}
+import NavItem from '../UI/NavItem';
 
 function Header() {
   const router = useRouter();
