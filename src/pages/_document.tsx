79c6230a821b449ec1711c4e5859998fd0f2394e
Facundo Martin <facundomartin98@gmail.com>
2022-09-15 08:16:05 -0700

    document fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 function Document() {
   return (
-    <Html lang="en">
+    <Html className="h-full antialiased" lang="en">
       <Head />
       <body className="flex h-full flex-col bg-zinc-50 dark:bg-zinc-800">
         <Main />
