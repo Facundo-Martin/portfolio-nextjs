e485d3b2e9e18068ef710edabdc7cc721da22d13
Facundo Martin <facundomartin98@gmail.com>
2022-09-14 17:16:33 -0700

    cleaned up header component and adjusted responsiveness

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,15 +1,10 @@
 import Link from 'next/link';
-import { useRouter } from 'next/router';
-import React, { useState } from 'react';
 import ThemeChanger from './ThemeChanger';
 import NavItem from '../UI/NavItem';
 import Image from 'next/image';
-import { Sling as Hamburger } from 'hamburger-react';
 import MobileNavigation from './MobileNavigation';
 
 function Header() {
-  const [isOpen, setIsOpen] = useState(false);
-  const router = useRouter();
   return (
     <div className="global-style">
       <div className="wrapper flex items-center justify-between py-10 bg-pink-100">
@@ -18,26 +13,18 @@ function Header() {
             <Image src="/vercel.svg" alt="profile image" width={20} height={20} layout="responsive" />
           </a>
         </Link>
-        <ul className="hidden lg:inline-flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
+        <ul className="hidden md:inline-flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
           <NavItem href="/about">About</NavItem>
           <NavItem href="/articles">Articles</NavItem>
           <NavItem href="/projects">Projects</NavItem>
           <NavItem href="/speaking">Speaking</NavItem>
         </ul>
-        <span className="hidden lg:block">
+        <span className="hidden md:block">
           <ThemeChanger />
         </span>
-        <div className="lg:hidden flex items-center gap-x-6">
+        <div className="md:hidden flex items-center gap-x-6">
           <ThemeChanger />
           <MobileNavigation />
-          {/* <Hamburger toggled={isOpen} toggle={setIsOpen} size={25} />
-          <div id="burger-menu" className={isOpen ? 'translate-x-0' : 'translate-x-full'}>
-            <div className="w-full">
-              {[1, 2, 3].map((navItem) => {
-                return <div>navItem</div>;
-              })}
-            </div>
-          </div> */}
         </div>
       </div>
     </div>
