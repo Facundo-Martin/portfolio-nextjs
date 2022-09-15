a89ab07bbe495422844aaea0aad8873d65f5fa9a
Facundo Martin <facundomartin98@gmail.com>
2022-09-15 08:05:13 -0700

    fixed header stlye

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -5,17 +5,15 @@ import DesktopNavigation from './DesktopNavigation';
 
 function Header() {
   return (
-    <div className="global-style">
-      <div className="wrapper flex items-center justify-between py-6 bg-pink-100">
-        <Link href="/">
-          <a className="w-10 hover:scale-110 transform transition ease-in-out duration-700">
-            <Image src="/vercel.svg" alt="profile image" width={20} height={20} layout="responsive" />
-          </a>
-        </Link>
-        <DesktopNavigation />
-        <MobileNavigation />
-      </div>
-    </div>
+    <header className="wrapper flex items-center justify-between py-6 bg-pink-100">
+      <Link href="/">
+        <a className="w-10 hover:scale-110 transform transition ease-in-out duration-700">
+          <Image src="/vercel.svg" alt="profile image" width={20} height={20} layout="responsive" />
+        </a>
+      </Link>
+      <DesktopNavigation />
+      <MobileNavigation />
+    </header>
   );
 }
 
