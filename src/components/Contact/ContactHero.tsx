d10c3ea0d62c9d8db0e5294bfc091880c47bfac6
Facundo Martin <facundomartin98@gmail.com>
2022-09-17 12:21:34 -0700

    finished adding ContactHero

--- src/components/Contact/ContactHero.tsx
+++ src/components/Contact/ContactHero.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
 function ContactHero() {
-  return <section className="section">ContactHero</section>;
+  return (
+    <section className="section px-4 bg-white mt-6">
+      <div className="">
+        <div className="relative z-10 mx-auto max-w-7xl pl-4 pr-8 sm:px-6 lg:px-8">
+          <h1 className="text-4xl font-bold tracking-tight text-warm-gray-900 sm:text-5xl lg:text-6xl">Get in touch</h1>
+          <p className="mt-6 max-w-3xl text-xl text-warm-gray-500">
+            Vel nunc non ut montes, viverra tempor. Proin lectus nibh phasellus morbi non morbi. In elementum urna ut volutpat. Sagittis et vel et fermentum
+            amet consequat.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default ContactHero;
