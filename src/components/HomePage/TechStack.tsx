56245295ee3aa86eccb266a53bd11d8f3d767286
Facundo Martin <facundomartin98@gmail.com>
2022-09-18 11:57:19 -0700

    changed techstack display from flex to grid

--- src/components/HomePage/TechStack.tsx
+++ src/components/HomePage/TechStack.tsx
@@ -17,26 +17,26 @@ const frameworks = [
 ];
 const tools = [
   { name: 'GraphQL', logo: logos.graphql },
-  { name: 'Apollo Server', logo: logos.apollographql },
+  { name: 'Apollo', logo: logos.apollographql },
   { name: 'Git', logo: logos.git },
   { name: 'Github', logo: logos.github },
   { name: 'Jira', logo: logos.jira },
   { name: 'VS Code', logo: logos.vscode },
 ];
 
-interface TechDataProps {
+interface TechLogoProps {
   tech: {
     name: string;
     logo: string;
   };
 }
-function TechData({ tech }: TechDataProps) {
+function TechLogo({ tech }: TechLogoProps) {
   return (
     <div className="flex flex-col items-center gap-y-1 opacity-80 hover:opacity-100 cursor-pointer hover:-translate-y-[6px] transition duration-700 group">
       <div className="w-14">
         <Image src={tech.logo} alt={`${tech.name} logo`} height={50} width={50} layout="responsive" className="rounded-sm" />
       </div>
-      <h4 className="text-base font-light dark:text-dark-text-secondary dark:group-hover:text-dark-text-primary dark:group-hover:opacity-85 mt-[2px]">
+      <h4 className="text-base text-center font-light dark:text-dark-text-secondary dark:group-hover:text-dark-text-primary dark:group-hover:opacity-85 mt-[2px]">
         {tech.name}
       </h4>
     </div>
@@ -47,25 +47,25 @@ function TechStack() {
     <div className="flex-1 space-y-6  max-w-xl rounded-xl px-6 py-5 border  border-neutral-100 dark:border-neutral-700/40 shadow-sm">
       <div>
         <h1 className="mb-3 font-medium text-zinc-700 dark:text-zinc-200">Programming Languages</h1>
-        <div className="flex gap-x-6 flex-wrap">
+        <div className="max-w-md grid grid-cols-2 md:grid-cols-4 md:gap-x-6 gap-y-6 pt-4">
           {programmingLanguages.map((tech) => (
-            <TechData tech={tech} />
+            <TechLogo tech={tech} />
           ))}
         </div>
       </div>
       <div>
         <h1 className="mb-3 font-medium text-zinc-700 dark:text-zinc-200">Frameworks</h1>
-        <div className="flex gap-x-6 flex-wrap">
+        <div className="max-w-md grid grid-cols-2 md:grid-cols-4 md:gap-x-6 gap-y-6 pt-4">
           {frameworks.map((tech) => (
-            <TechData tech={tech} />
+            <TechLogo tech={tech} />
           ))}
         </div>
       </div>
       <div>
         <h1 className="mb-3 font-medium text-zinc-700 dark:text-zinc-200">Tools of the trade</h1>
-        <div className="flex gap-x-6 flex-wrap">
+        <div className="max-w-md grid grid-cols-2 md:grid-cols-4 md:gap-x-6 gap-y-6 pt-4">
           {tools.map((tech) => (
-            <TechData tech={tech} />
+            <TechLogo tech={tech} />
           ))}
         </div>
       </div>
