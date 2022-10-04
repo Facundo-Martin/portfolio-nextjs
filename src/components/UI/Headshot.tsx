f39c9a57fcd2668e9b5c5176cb9a13a9c5d695b5
Facundo Martin <facundomartin98@gmail.com>
2022-10-04 11:07:27 -0700

    fixed Headshot component

--- src/components/UI/Headshot.tsx
+++ src/components/UI/Headshot.tsx
@@ -3,13 +3,11 @@ import Image from 'next/image';
 import Link from 'next/link';
 import photos from '../../assets/photos/index';
 
-function Headshot({ width }: { width: number }) {
-  const divWidth = 'w-' + width.toString();
-  const divHeght = 'h-' + width.toString();
+function Headshot({ size }: { size?: 'lg' }) {
   return (
     <Link href="/about">
-      <a className={divHeght}>
-        <div className={clsx(divWidth, 'rounded-full mb-4 bg-black hover:scale-110 transform transition ease-in-out duration-700')}>
+      <a className={size == 'lg' ? 'h-20' : 'h-10'}>
+        <div className={clsx(size == 'lg' ? 'w-20' : 'w-10', 'rounded-full mb-4 bg-black hover:scale-110 transform transition ease-in-out duration-700')}>
           <Image src={photos.headshot} alt="Facundo Martin logo image" width={50} height={50} layout="responsive" className="rounded-full" />
         </div>
       </a>
