d054526dc5c2be202797159d8248ce5ac7831140
Facundo Martin <facundomartin98@gmail.com>
2022-10-04 16:20:29 -0700

    added visit website button on Project page image

--- src/components/Project/ProjectMain.tsx
+++ src/components/Project/ProjectMain.tsx
@@ -8,7 +8,10 @@ function ProjectMain({ project }: { project: Project }) {
     <section className="section pt-0">
       <Card className="max-w-none flex gap-x-10">
         <a href={project.url} target="_blank" rel="noreferrer" className="relative group">
-          <Image src={project.preview} width={1200} height={1000} className="rounded-lg" />
+          <div className="hidden group-hover:grid absolute inset-0 place-items-center z-10">
+            <div className="px-4 py-2 bg-white rounded-lg text-gray-600 font-semibold">Visit Website</div>
+          </div>
+          <Image src={project.preview} width={1200} height={1000} className="rounded-lg group-hover:opacity-60" />
         </a>
         <div>
           <div>{project.description}</div>
