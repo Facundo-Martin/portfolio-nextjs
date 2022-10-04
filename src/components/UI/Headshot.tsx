6b00e67e983e80e20902661d1878ec90367850b9
Facundo Martin <facundomartin98@gmail.com>
2022-10-04 10:38:19 -0700

    fixed headshot width

--- src/components/UI/Headshot.tsx
+++ src/components/UI/Headshot.tsx
@@ -6,7 +6,7 @@ import photos from '../../assets/photos/index';
 function Headshot({ width }: { width: number }) {
   return (
     <Link href="/about">
-      <a className={`h-${width}`}>
+      <a className="h-10">
         <div className={clsx(`w-${width}`, 'rounded-full mb-4 bg-black hover:scale-110 transform transition ease-in-out duration-700')}>
           <Image src={photos.headshot} alt="Facundo Martin logo image" width={50} height={50} layout="responsive" className="rounded-full" />
         </div>
