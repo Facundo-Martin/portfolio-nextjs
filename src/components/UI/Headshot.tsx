c8481097dc5db0a41631ec1df9e03e97234f1c0f
Facundo Martin <facundomartin98@gmail.com>
2022-10-04 11:08:41 -0700

    Headshot fix

--- src/components/UI/Headshot.tsx
+++ src/components/UI/Headshot.tsx
@@ -4,10 +4,12 @@ import Link from 'next/link';
 import photos from '../../assets/photos/index';
 
 function Headshot({ size }: { size?: 'lg' }) {
+  const divWidth = size == 'lg' ? 'w-20' : 'w-10';
+  const divHeight = size == 'lg' ? 'h-20' : 'h-10';
   return (
     <Link href="/about">
-      <a className={size == 'lg' ? 'h-20' : 'h-10'}>
-        <div className={clsx(size == 'lg' ? 'w-20' : 'w-10', 'rounded-full mb-4 bg-black hover:scale-110 transform transition ease-in-out duration-700')}>
+      <a className={divHeight}>
+        <div className={clsx(divWidth, 'rounded-full mb-4 bg-black hover:scale-110 transform transition ease-in-out duration-700')}>
           <Image src={photos.headshot} alt="Facundo Martin logo image" width={50} height={50} layout="responsive" className="rounded-full" />
         </div>
       </a>
