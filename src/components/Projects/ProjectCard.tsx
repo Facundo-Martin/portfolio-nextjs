0fd9aab11c07633b74f1039f4a8df6bc4955957c
Facundo Martin <facundomartin98@gmail.com>
2022-10-04 13:32:10 -0700

    created Card UI component

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Project } from '../../d';
+import Card from '../UI/Card';
 
 type ProjectCardProps = {
   project: Project;
 };
 function ProjectCard({ project }: ProjectCardProps) {
-  return <div>ProjectCard</div>;
+  return (
+    <Card className="space-y-2">
+      <div>Image</div>
+      <div>Title</div>
+      <div>Description</div>
+      <div>TechStack</div>
+      <div>Read More</div>
+    </Card>
+  );
 }
 
 export default ProjectCard;
