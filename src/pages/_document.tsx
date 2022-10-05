9cff69a7cd47f746aac5fe27177caf387f9ca370
Facundo Martin <facundomartin98@gmail.com>
2022-10-05 08:02:39 -0700

    added global text styles for dark and light mode

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ function Document() {
   return (
     <Html className="h-full antialiased" lang="en">
       <Head />
-      <body className="flex h-full flex-col bg-zinc-50 dark:bg-black ">
+      <body className="flex h-full flex-col bg-zinc-50 dark:bg-black text-black dark:text-white ">
         <Main />
         <NextScript />
       </body>
