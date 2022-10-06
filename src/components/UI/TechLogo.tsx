6f82b47fc4ae73f4ffc313ab42ff3edb3227ef52
Facundo Martin <facundomartin98@gmail.com>
2022-10-06 08:17:47 -0700

    added TechLogo component

--- src/components/UI/TechLogo.tsx
+++ src/components/UI/TechLogo.tsx
@@ -1,15 +1,26 @@
+import Image from 'next/image';
 import React from 'react';
 import { techStack } from '../../utils';
 
 type TechLogoProps = {
-  tech: string;
+  name: string;
 };
-function TechLogo({ tech }: TechLogoProps) {
-  let techData = techStack.find((x) => x.name === tech);
 
-  if (!techData) return;
+function TechLogo({ name }: TechLogoProps) {
+  let tech = techStack.find((x) => x.name === name);
 
-  return <div>TechLogo</div>;
+  if (!tech) return;
+
+  return (
+    <div className="flex flex-col items-center gap-y-1 opacity-80 hover:opacity-100 cursor-pointer hover:-translate-y-[6px] transition duration-700 group">
+      <div className="w-14">
+        <Image src={tech.logo} alt={`${tech.name} logo`} height={50} width={50} layout="responsive" className="rounded-sm" />
+      </div>
+      <h4 className="text-base text-center font-light dark:text-dark-text-secondary dark:group-hover:text-dark-text-primary dark:group-hover:opacity-85 mt-[2px]">
+        {tech.name}
+      </h4>
+    </div>
+  );
 }
 
 export default TechLogo;
