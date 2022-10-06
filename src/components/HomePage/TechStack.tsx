508e30dffeedeffd95a72e3f2299077fbcdcce01
Facundo Martin <facundomartin98@gmail.com>
2022-10-06 15:24:41 -0700

    changed grid layout to flex

--- src/components/HomePage/TechStack.tsx
+++ src/components/HomePage/TechStack.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 // Programming Languages
 import { techStack } from '../../utils';
 import TechLogo from '../TechLogo';
+import Card from '../UI/Card';
 
 const styles = {
   wrapper: 'border-b pb-6 border-zinc-200 dark:border-zinc-600/50 group',
   title: 'mb-3 font-medium text-zinc-700 dark:text-zinc-200',
-  grid: 'max-w-md grid grid-cols-4 md:gap-x-6 gap-y-6 pt-3',
+  flex: 'flex items-center gap-x-10 pt-3',
 };
 
 function TechStack() {
@@ -14,10 +15,10 @@ function TechStack() {
   const frameworks = techStack.filter((x) => x.category == 'Framework');
   const tools = techStack.filter((x) => x.category == 'Tool');
   return (
-    <div className="flex-1 space-y-6  max-w-xl rounded-xl px-6 py-5 border  border-neutral-100 dark:border-neutral-700/40 shadow-sm">
+    <Card className="flex-1 space-y-6 max-w-lg">
       <div className={styles.wrapper}>
         <h1 className={styles.title}>Programming Languages</h1>
-        <div className={styles.grid}>
+        <div className={styles.flex}>
           {languages.map((tech) => (
             <TechLogo tech={tech} />
           ))}
@@ -25,7 +26,7 @@ function TechStack() {
       </div>
       <div className={styles.wrapper}>
         <h1 className={styles.title}>Frameworks</h1>
-        <div className={styles.grid}>
+        <div className={styles.flex}>
           {frameworks.map((tech) => (
             <TechLogo tech={tech} />
           ))}
@@ -33,13 +34,13 @@ function TechStack() {
       </div>
       <div className="group">
         <h1 className={styles.title}>Tools of the trade</h1>
-        <div className={styles.grid}>
+        <div className={styles.flex}>
           {tools.map((tech) => (
             <TechLogo tech={tech} />
           ))}
         </div>
       </div>
-    </div>
+    </Card>
   );
 }
 
