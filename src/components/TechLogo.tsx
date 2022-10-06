fcc4850092d360976c920d924f1df81bc7d9862f
Facundo Martin <facundomartin98@gmail.com>
2022-10-06 08:49:39 -0700

    created size variant for TechLogo

--- src/components/TechLogo.tsx
+++ src/components/TechLogo.tsx
@@ -1,18 +1,25 @@
+import clsx from 'clsx';
 import Image from 'next/image';
 import React from 'react';
 import { TechData } from '../d';
 
 type TechLogoProps = {
   tech: TechData;
+  size?: string;
 };
 
-function TechLogo({ tech }: TechLogoProps) {
+function TechLogo({ tech, size = 'sm' }: TechLogoProps) {
   return (
     <div className="flex flex-col items-center gap-y-1 opacity-80 hover:opacity-100 cursor-pointer hover:-translate-y-[6px] transition duration-700 group">
-      <div className="w-14">
+      <div className={size == 'sm' ? 'w-12' : 'w-14'}>
         <Image src={tech.logo} alt={`${tech.name} logo`} height={50} width={50} layout="responsive" className="rounded-sm" />
       </div>
-      <h4 className="text-base text-center font-light dark:text-dark-text-secondary dark:group-hover:text-dark-text-primary dark:group-hover:opacity-85 mt-[2px]">
+      <h4
+        className={clsx(
+          size == 'sm' ? 'text-sm' : 'text-base',
+          'text-center font-light dark:text-dark-text-secondary dark:group-hover:text-dark-text-primary dark:group-hover:opacity-85 mt-[2px]'
+        )}
+      >
         {tech.name}
       </h4>
     </div>
