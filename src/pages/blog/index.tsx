40449ef681cd7d138f864a4b4874b352aa774ab6
Facundo-Martin <facundomartin98@gmail.com>
2022-10-25 15:19:51 -0700

    added dummy posts component

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,7 +1,9 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import RenderBlog from '../../components/Blog/RenderBlog';
 import Layout from '../../components/Layout/Layout';
 
+const posts = [{ title: 'Facundo Martin', excerpt: 'lalala' }];
 const Blog: NextPage = () => {
   return (
     <>
@@ -9,7 +11,7 @@ const Blog: NextPage = () => {
         <title> About | Facundo Martin</title>
       </Head>
       <Layout>
-        <h1>Blog</h1>
+        <RenderBlog posts={posts} />
       </Layout>
     </>
   );
