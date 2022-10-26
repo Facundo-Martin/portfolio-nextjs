11e5b1fd2fa876d173891621750ffb87079bacd8
Facundo Martin <facundomartin98@gmail.com>
2022-10-26 11:46:53 -0700

    modal style

--- src/components/Blog/SearchModal.tsx
+++ src/components/Blog/SearchModal.tsx
@@ -9,14 +9,14 @@ type ModalProps = {
 };
 function Modal({ isOpen, setIsOpen }: ModalProps) {
   return (
-    <Dialog open={isOpen} onClose={() => setIsOpen(false)} className="relative z-50">
+    <Dialog open={isOpen} onClose={() => setIsOpen(false)} className="relative z-50 ">
       {/* The backdrop, rendered as a fixed sibling to the panel container */}
       <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
 
       {/* Full-screen container to center the panel */}
-      <div className="fixed inset-0 flex items-center justify-center p-4">
+      <div className="fixed inset-0 flex items-center justify-center p-4 ">
         {/* The actual dialog panel  */}
-        <Dialog.Panel className="mx-auto max-w-sm rounded bg-white">
+        <Dialog.Panel className="mx-auto w-full max-w-3xl rounded bg-white h-[50vh]">
           <Dialog.Title>Complete your order</Dialog.Title>
 
           {/* ... */}
